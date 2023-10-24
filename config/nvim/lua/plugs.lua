6acb22d39319d83256bf34430e78abf160329f9b
Bo Cao <caobo.86.mail@gmail.com>
2023-10-24 09:50:48 +0800

    update indent-blankline plugin for nvim.

--- config/nvim/lua/plugs.lua
+++ config/nvim/lua/plugs.lua
@@ -83,18 +83,6 @@ require('lazy').setup({
         }
     },
 
-    { -- Add indentation guides even on blank lines
-        'lukas-reineke/indent-blankline.nvim',
-        event = { 'BufReadPost', 'BufNewFile' },
-        -- Enable `lukas-reineke/indent-blankline.nvim`
-        -- See `:help indent_blankline.txt`
-        opts = {
-            char = '┊',
-            -- show_trailing_blankline_indent = false,
-            show_trailing_blankline_indent = true,
-        }
-    },
-
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim',
         event = { 'BufReadPost', 'BufNewFile' },

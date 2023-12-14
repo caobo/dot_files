cca5f502e58497c2d119f5b0f4a68dd840d092af
Bo Cao <caobo.86.mail@gmail.com>
2023-12-14 10:09:11 +0800

    add imgcat for image preview.

--- config/lf/preview.sh
+++ config/lf/preview.sh
@@ -3,5 +3,6 @@
 case "$1" in
 *.tar*) tar tf "$1" ;;
 *.zip) unzip -l "$1" ;;
+*.jpg|*.jpeg|*.png) imgcat -r 50 -d true "$1" && read -n 1 -s -r;;
 *) bat --force-colorization --paging=never --style=numbers --terminal-width $(($2 - 5)) -f "$1" ;;
 esac

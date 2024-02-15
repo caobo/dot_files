2821da13e5cc0cd2f19380ae77bbbad91c2b0540
Bo Cao <caobo.86.mail@gmail.com>
2024-02-15 10:52:25 +0800

    minor tweaks.

--- config/nvim/lua/custom/plugins/surround.lua
+++ config/nvim/lua/custom/plugins/surround.lua
@@ -3,9 +3,11 @@ local M = {'kylechui/nvim-surround'}
 M.event ={ 'BufReadPost', 'BufNewFile'}
 
 function M.config()
-    require('nvim-surround').setup({
+    require('nvim-surround').setup(
+    {
         -- Configuration here, or leave empty to use defaults
-    })
+    }
+    )
 end
 
 return M

f6f70736e76365de32870cf8dc6ab5d53b4a2a1d
Bo Cao <caobo.86.mail@gmail.com>
2024-03-13 13:27:38 +0800

    settings for wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,90 +1,44 @@
-local wezterm = require 'wezterm'
-local mux = wezterm.mux
+local wezterm = require('wezterm')
 local colors = require('lua/theme/rose-pine-moon').colors()
 local window_frame = require('lua/theme/rose-pine-moon').window_frame()
-
-wezterm.on('gui-startup', function(cmd)
-    local _, _, window = mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
-end)
-
+local gpus = wezterm.gui.enumerate_gpus()
 local font = wezterm.font { family = 'MonoLisa Medium' }
 
-local font_rules = {
-  {
-    italic = true,
-    intensity = 'Bold',
-    font = wezterm.font {
-      family = 'MonoLisa',
-      weight = 'Bold',
-      style = 'Italic',
+local M = {
+    -- environment variable
+    set_environment_variables = {
+        TERM = "xterm-256color",
+        LC_ALL = "en_US.UTF-8",
     },
-  },
-{
-    italic = false,
-    intensity = 'Bold',
-    font = wezterm.font {
-      family = 'MonoLisa',
-      weight = 'Bold',
-    },
-  },
-  {
-    italic = true,
-    intensity = 'Half',
-    font = wezterm.font {
-      family = 'MonoLisa',
-      weight = 'DemiBold',
-      style = 'Italic',
-    },
-  },
-  {
-    italic = false,
-    intensity = 'Half',
-    font = wezterm.font {
-      family = 'MonoLisa',
-      weight = 'DemiBold',
-    },
-  },
-  {
-    italic = true,
-    intensity = 'Normal',
-    font = wezterm.font {
-      family = 'MonoLisa',
-      weight = 'Medium',
-      style = 'Italic',
-    },
-  },
-  {
-    italic = false,
-    intensity = 'Normal',
-    font = wezterm.font {
-      family = 'MonoLisa',
-      weight = 'Medium',
-    },
-  },
-}
-
-return {
+    -- fonts
     font = font,
-    font_rules = font_rules,
     font_size = 16,
     freetype_load_target = "Normal",
     line_height = 1.2,
-    colors = colors,
-    window_frame = window_frame, -- needed only if using fancy tab bar
+    -- window and tab
+    window_frame = window_frame,
     window_decorations = "RESIZE|MACOS_FORCE_DISABLE_SHADOW",
+    window_padding = { left = 5, right = 5, top = 5, bottom = 5},
+    hide_tab_bar_if_only_one_tab = true,
+    use_fancy_tab_bar = false,
+    -- appearence
+    colors = colors,
     window_background_opacity = 1,
     text_background_opacity = 1,
-    window_padding = { left = 5, right = 5, top = 5, bottom = 5},
-    use_fancy_tab_bar = false,
-    tab_and_split_indices_are_zero_based = true,
-    audible_bell="Disabled",
-    send_composed_key_when_left_alt_is_pressed=true,
-    max_fps = 200,
-    animation_fps = 120,
-    hide_tab_bar_if_only_one_tab = true,
     default_cursor_style = 'BlinkingBar',
     cursor_thickness = 6.0,
     force_reverse_video_cursor = true,
+    -- preformance
+    webgpu_preferred_adapter = gpus[1],
+    front_end = "WebGpu",
+    webgpu_power_preference = "HighPerformance",
+    max_fps = 200,
+    animation_fps = 120,
+    -- others
+    tab_and_split_indices_are_zero_based = true,
+    audible_bell="Disabled",
+    send_composed_key_when_left_alt_is_pressed=true,
     enable_kitty_graphics = true,
 }
+
+return M

7952d3a4838866a901d672fd58ddcdcf9831372f
Bo Cao <caobo.86.mail@gmail.com>
2024-04-09 11:48:56 +0800

    minor tweaks on lualine.

--- config/nvim/lua/custom/plugins/lualine.lua
+++ config/nvim/lua/custom/plugins/lualine.lua
@@ -15,35 +15,44 @@ function M.config()
     end
 
     local lualine = require("lualine")
+
     lualine.setup({
         options = {
             theme = "auto",
             icons_enabled = true,
             disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
-            -- component_separators = { left = '', right = '' },
             -- section_separators = { left = "|", right = "|" },
             component_separators = " ",
-            section_separators = { left = '', right = '' },
+            -- component_separators = "|",
+            section_separators = { left = '▊', right = '▊' },
         },
         sections = {
-            lualine_a = { { "mode", separator = { left = "[" }, right_padding = 3 } },
+            lualine_a = { { "mode", separator = { left = "▊" }, right_padding = 3 } },
             lualine_b = {'filename', "branch", 'diagnostics', { "diff", colored = true } },
-            lualine_c = {
+            lualine_c = { "searchcount", "selectioncount",
                 {'buffers',
                     show_filename_only = true,   -- Shows shortened relative path when set to false.
                     hide_filename_extension = false,   -- Hide filename extension when set to true.
                     show_modified_status = true, -- Shows indicator when the buffer is modified.
-                    mode = 4,
+                    mode = 1,
                     -- 0: Shows buffer name
                     -- 1: Shows buffer index
                     -- 2: Shows buffer name + buffer index
                     -- 3: Shows buffer number
                     -- 4: Shows buffer name + buffer number
-                }
+                },
+                use_mode_colors = false,
             },
-            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_x = { 'encoding',
+                {'fileformat',
+                    symbols = { unix = 'Unix', -- e712
+                            dos = 'Dos',  -- e70f
+                            mac = 'Mac',  -- e711
+                    }
+                },
+                'filetype'},
             lualine_y = { progress,"progress" },
-            lualine_z = { { "location", separator = { right = "]" }, left_padding = 3 } },
+            lualine_z = { { "location", separator = { right = "▊" }, left_padding = 3 } },
         },
         inactive_sections = {
             lualine_a = { "filename" },

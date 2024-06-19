f345e20e1659fd539b212813286d030fc57fc7c2
Bo Cao <caobo.86.mail@gmail.com>
2024-06-19 10:06:04 +0800

    add: markdown previewer in neovim.

---
+++ config/nvim/lua/custom/plugins/markdown.lua
@@ -0,0 +1,10 @@
+local M ={'MeanderingProgrammer/markdown.nvim'}
+M.dependencies = {'nvim-treesitter/nvim-treesitter'}
+
+M.ft={"markdown"}
+
+M.config = function()
+    require('render-markdown').setup({})
+end
+
+return M

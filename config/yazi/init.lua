5bcdb5fe1a10db55310537e4058163a841035296
Bo Cao <caobo.86.mail@gmail.com>
2024-12-17 11:26:03 +0800

    modify: add new yazi plugin

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,1 +1,6 @@
 require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
+
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}

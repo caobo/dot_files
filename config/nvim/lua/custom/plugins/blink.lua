95c17d3d7fa3b5101cd7eb600b72069b8c70eb19
Bo Cao <caobo.86.mail@gmail.com>
2024-12-20 14:20:42 +0800

    modify: blink.cmp settings.

--- config/nvim/lua/custom/plugins/blink.lua
+++ config/nvim/lua/custom/plugins/blink.lua
@@ -19,8 +19,19 @@ function M.config ()
       nerd_font_variant = 'mono'
     },
 
+    snippets = {
+      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require('luasnip').jumpable(filter.direction)
+        end
+        return require('luasnip').in_snippet()
+      end,
+      jump = function(direction) require('luasnip').jump(direction) end,
+    },
+
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'luasnipe', 'snippets', 'buffer' },
       -- optionally disable cmdline completions
       -- cmdline = {},
     },

9a8fa9ba853c027fb1eaab4952d4d1c11f7bbeb3
Bo Cao <caobo.86.mail@gmail.com>
2025-02-18 11:30:01 +0800

    Modify: markdown render settings

--- config/nvim/lua/custom/plugins/markdown.lua
+++ config/nvim/lua/custom/plugins/markdown.lua
@@ -4,7 +4,25 @@ M.dependencies = { 'nvim-treesitter/nvim-treesitter' }
 M.ft = { 'markdown' }
 
 M.config = function()
-    require('render-markdown').setup({})
+    require('render-markdown').setup({
+        heading = {
+            width = 'block'
+        },
+        code = {
+            width = "block",
+        },
+        checkbox = {
+            checked = { scope_highlight = "@markup.strikethrough" },
+            custom = {
+                todo = { raw = "", rendered = "", highlight = "" },
+                canceled = {
+                    raw = "[-]",
+                    rendered = "󱘹",
+                    scope_highlight = "@markup.strikethrough",
+                },
+            },
+        },
+    })
 end
 
 return M

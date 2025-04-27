7c35bf72ad9353ad79c1096221af80c09897af45
Bo Cao <caobo.86.mail@gmail.com>
2025-04-27 10:06:18 +0800

    Modify: disable rocks in lazynvim

--- config/nvim/lua/plugs.lua
+++ config/nvim/lua/plugs.lua
@@ -1,14 +1,17 @@
 -- [[ plug.lua ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-    { import = 'custom/plugins' },
+    spec = {
+        { import = 'custom/plugins' },
+    },
+    rocks = { enabled = false },
 })

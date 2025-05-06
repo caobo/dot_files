d6abf7f4382f5823aac94eee1427aa9c17fb12dc
Bo Cao <caobo.86.mail@gmail.com>
2025-05-06 10:07:46 +0800

    Modify: disable inlay hint in nvim

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -106,7 +106,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local map = vim.keymap.set
         local opts = { buffer = event.buf, remap = false }
         map("n", "gd", function() vim.lsp.buf.definition() end, { desc = "Goto definition (lsp)", unpack(opts) })
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        -- vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
         vim.diagnostic.config({
             signs = {
                 text = {
